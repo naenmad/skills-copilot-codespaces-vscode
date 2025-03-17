fa962c2e42994aa63e3a2f6cc612013a68ff2c59
Ahmad Zulkarnaen <145246990+naenmad@users.noreply.github.com>
2025-03-17 21:12:01 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,46 +1,27 @@
 // Create Web Server
-// Create a web server that listens on port 3000. When you navigate to http://localhost:3000/ in your browser, the server should respond with the following content:
+const express = require('express');
+const app = express();
+const port = 3000;
 
-// <!DOCTYPE html>
-// <html>
-// <head>
-//   <title>Comments</title>
-// </head>
-// <body>
-//   <h1>Comments</h1>
-//   <ul>
-//     <li>Comment 1</li>
-//     <li>Comment 2</li>
-//     <li>Comment 3</li>
-//     <li>Comment 4</li>
-//     <li>Comment 5</li>
-//   </ul>
-// </body>
-// </html>
+// Create a JSON body parser
+app.use(express.json());
 
-const http = require('http');
+// Create a comments array
+const comments = [];
 
-const server = http.createServer((req, res) => {
-  res.setHeader('Content-Type', 'text/html');
-  res.write('<!DOCTYPE html>');
-  res.write('<html>');
-  res.write('<head>');
-  res.write('<title>Comments</title>');
-  res.write('</head>');
-  res.write('<body>');
-  res.write('<h1>Comments</h1>');
-  res.write('<ul>');
-  res.write('<li>Comment 1</li>');
-  res.write('<li>Comment 2</li>');
-  res.write('<li>Comment 3</li>');
-  res.write('<li>Comment 4</li>');
-  res.write('<li>Comment 5</li>');
-  res.write('</ul>');
-  res.write('</body>');
-  res.write('</html>');
-  res.end();
+// Create a GET request to get all comments
+app.get('/comments', (req, res) => {
+  res.json(comments);
 });
 
-server.listen(3000, () => {
-  console.log('Server is listening on port 3000');
+// Create a POST request to add a comment
+app.post('/comments', (req, res) => {
+  const { username, comment } = req.body;
+  comments.push({ username, comment });
+  res.json({ username, comment });
+});
+
+// Listen to the port
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
