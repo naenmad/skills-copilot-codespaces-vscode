8cd5c679f6a2c8ea84aa85478585c9191e6bfe0c
Ahmad Zulkarnaen <145246990+naenmad@users.noreply.github.com>
2025-03-17 21:16:20 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,61 +1,31 @@
 // Create Web Server
-const express = require('express');
-const app = express();
+const http = require('http');
 
-// Create Middleware
-app.use(express.json());
-
-// Create Array
-const comments = [
-    {message: 'Hello'},
-    {message: 'Hi'}
-];
-
-// Create Route
-app.get('/comments', (req, res) => {
-    res.send(comments);
+const server = http.createServer((req, res) => {
+  res.setHeader('Content-Type', 'text/html');
+  res.write('<!DOCTYPE html>');
+  res.write('<html>');
+  res.write('<head>');
+  res.write('<title>Comments</title>');
+  res.write('</head>');
+  res.write('<body>');
+  res.write('<h1>Comments</h1>');
+  res.write('<ul>');
+  res.write('<li>Comment 1</li>');
+  res.write('<li>Comment 2</li>');
+  res.write('<li>Comment 3</li>');
+  res.write('<li>Comment 4</li>');
+  res.write('<li>Comment 5</li>');
+  res.write('</ul>');
+  res.write('</body>');
+  res.write('</html>');
+  res.end();
 });
 
-// Create Route
-app.post('/comments', (req, res) => {
-    const comment = {message: req.body.message};
-    comments.push(comment);
-    res.send(comment);
+server.on('error', (error) => {
+  console.error('Server error:', error);
 });
 
-// Create Route
-app.get('/comments/:id', (req, res) => {
-    const comment = comments[req.params.id];
-    res.send(comment);
+server.listen(3000, () => {
+  console.log('Server is listening on port 3000');
 });
-
-// Create Route
-app.put('/comments/:id', (req, res) => {
-    const comment = comments[req.params.id];
-    comment.message = req.body.message;
-    res.send(comment);
-});
-
-// Create Route
-app.delete('/comments/:id', (req, res) => {
-    comments.splice(req.params.id, 1);
-    res.send(comments);
-});
-
-// Create Port
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
-});
-
-// Run Server
-// node comments.js
-// npm install nodemon -g
-// nodemon comments.js
-// npm install express
-// npm install body-parser
-// npm install nodemon -g
-// npm install express
-// npm install body-parser
-// nodemon comments.js
-// npm install express
-// npm install body-parser
